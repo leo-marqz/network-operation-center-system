83a9044bd6a2d3fd9f208f3ba09f49f29995ee5e
Elmer Marquez <leomarqz2020@gmail.com>
2025-10-05 01:10:52 -0600

    docs: probando la libreria cron

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,6 +1,17 @@
 
+import color from 'picocolors';
+
+import { CronJob } from 'cron';
+
 export class Server {
     public static start(){
-        console.log('Server started');
+        console.log(`${color.green('[INFO]')} Server is running...`);
+
+        let job = new CronJob('*/2 * * * * *', ()=>{
+            console.log("You will see this message every two second...", new Date());
+        });
+
+        job.start();
+
     }
 }
